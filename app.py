945f66ad414a4ac5375b0128571a8567555e505a
Erick-Faster <erick.fasterra@gmail.com>
2020-07-13 00:06:46 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+# Configure application
+app = Flask(__name__)
+
+@app.route('/index', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=5060, debug=True)
